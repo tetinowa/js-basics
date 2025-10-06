a580e5fc95a0a1ee09860c5eb12ea0511abb1900
tetinowa <tmrgaunt@example.com>
2025-10-06 12:53:51 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+// function isDivisibleby5(a) {
+//     if (a % 5 === 0 )
+//         return true;
+//     else {return false}
+// }
+
+// console.log(isDivisibleby5(47));
+
+// function second(a) {
+//     if (a >=1 && a<=100) return "include";
+//     else return "exclude";
+// }
+
+// console.log(second(20));
+
+// function third(a,b,c) {
+//     if (a > b && a > c) return a;
+//     if (b > a && b > c) return b;
+//     if (c > a && c > b) return c;
+// }
+// console.log(third(23,56,22))
+
+function fourth(a) {
+    if (a % 4 ==0) return true; else return false;
+}
+
+console.log(fourth(2007))
