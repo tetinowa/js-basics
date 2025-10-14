85cea9e31cc5f0b7b936db7238520dc5d3458a44
tetinowa <tmrgaunt@example.com>
2025-10-14 12:39:50 +0800

    awawa

---
+++ 6.js
@@ -0,0 +1,28 @@
+// const char = {
+//   name: "Arthur Nightingale",
+//   gender: "Male",
+//   age: "37",
+//   isaProtoframe: true,
+//   build: {
+//     sur: "Nourish",
+// crit: "Wrathful_Advance",
+//   },
+//   afilliation: "The Hex",
+// };
+
+// console.log(char["build"][1]);
+
+// filter, find, include
+
+const strr = "hello";
+
+const reversed = (strr) => {
+  return strr.split("").reverse().join("");
+  //   .reverse().join("");
+};
+
+console.log(reversed(strr));
+
+const checkinpali = "lever";
+
+const pali = (checkinpali) => {};
