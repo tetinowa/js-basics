e2bd611b9641a54be996bb996c1850bd46d2fccd
tetinowa <tmrgaunt@example.com>
2025-10-17 11:04:49 +0800

    ahshhahhh

---
+++ candy-shop-lul/script.js
@@ -0,0 +1,14 @@
+let BigText = document.getElementById("BigText");
+const CandyBtn = document.getElementById("Candy");
+const CakeBtn = document.getElementById("Cake");
+const CreamBtn = document.getElementById("Cream");
+
+function CandyDisp() {
+  BigText.textContent = "🍭 Таны чихрийн захиалга баталгаажлаа";
+}
+function CakeDisp() {
+  BigText.textContent = "🍰 Таны бялууны захиалгыг хүлээн авлаа";
+}
+function CreamDisp() {
+  BigText.textContent = "🍦 Зайрмаг удахгүй ирнэ";
+}
