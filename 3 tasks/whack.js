9496095515c2c256a34bc6196b88e637d1d37587
tetinowa <tmrgaunt@example.com>
2025-10-31 12:30:32 +0800

    aaahahha

--- 3 tasks/whack.js
+++ 3 tasks/whack.js
@@ -2,16 +2,22 @@ const startButton = document.querySelector(".startButton");
 const moles = document.querySelectorAll(".hole img");
 let score = 0;
 const scoreBoard = document.querySelector(".score");
-
+let click = 0;
+shown = false;
 startButton.addEventListener("click", () => {
   setInterval(() => {
-    const randomIndex = Math.floor(Math.random() * moles.length);
-    const mole = moles[randomIndex];
-    mole.style.bottom = "0";
+    if (!shown) {
+      shown = true;
 
-    setTimeout(() => {
-      mole.style.bottom = "100%";
-    }, Math.random() * 1000 + 500);
+      const randomIndex = Math.floor(Math.random() * moles.length);
+      const mole = moles[randomIndex];
+      mole.style.bottom = "0";
+      mole.style.pointerEvents = "auto";
+      setTimeout(() => {
+        mole.style.bottom = "100%";
+        shown = false;
+      }, Math.random() * 1000 + 500);
+    }
   }, Math.random() * 1000 + 500);
 });
 
@@ -21,7 +27,9 @@ render = () => {
 
 moles.forEach((mole) => {
   mole.addEventListener("click", () => {
+    click++;
     score++;
     render();
+    mole.style.pointerEvents = "none";
   });
 });
